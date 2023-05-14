9747d28e010b70f0e99b36f70bd930cb4284e5b8
Chris Gibbons <christ487@icloud.com>
2023-05-14 16:51:58 +0100

    added firebase to JS, added JS to app

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js" 
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://todolist-d0028-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+console.log(app)
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl = addEventListener("click", function() {
-    console.log("it works");
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
+
+    appendItemToShoppingListEl(inputValue)
 })
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
+
