113579cb09205df36618017adf4ff2cf85745fc8
Chris Gibbons <christ487@icloud.com>
2023-05-14 17:03:30 +0100

    added a side challenge for object into array

--- index.js
+++ index.js
@@ -34,3 +34,20 @@ function appendItemToShoppingListEl(itemValue) {
     shoppingListEl.innerHTML += `<li>${itemValue}</li>`
 }
 
+
+
+// Turning an object into an Array ------------------------------------
+
+// let scrimbaUsers = {
+//     "00": "sindre@scrimba.com",
+//     "01": "per@scrimba.com",
+//     "02": "frode@scrimba.com"
+// }
+
+// let scrimbaUsersEmails = Object.values(scrimbaUsers)
+
+// let scrimbaUsersIDs = Object.keys(scrimbaUsers)
+
+// let scrimbaUsersEntries = Object.entries(scrimbaUsers)
+
+// console.log(scrimbaUsersEntries)
